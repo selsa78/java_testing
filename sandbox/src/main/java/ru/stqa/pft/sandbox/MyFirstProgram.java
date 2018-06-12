8860c93334f8e56c7c5d28501c77014dfccbf10c
Sergey Selivanov <selsa@mail.ru>
2018-06-12 18:05:08 +0300

    Вынос мозга (Задание 1)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,26 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-	public static void main(String[] args) {
-		System.out.println ("Hello, world!!!");
+	public static void main(String[] args) {			// Функция main прнимает на вход массив строк
+		hello("world");										// Обращение к функции hello с конкретным значением для параметра somebody
+		hello("user");
+		hello("Sergey");
+
+		Square s = new Square(5); 			// Создание объекта заданного типа
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+
+		Rectangle r = new Rectangle(4, 6);
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+	}
+	public static void hello(String somebody){		// Функция, которая выполняет команду вывода на экран текста с ее параметром somebody
+		System.out.println ("Hello, " + somebody + "!!!");		// В кавычках - литеральное значение
+	}
+
+	public static double area(Square s){
+		return s.l * s.l;
+	}
+
+	public static double area (Rectangle r){
+		return r.a * r.b;
 	}
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
@@ -0,0 +1,10 @@
+package ru.stqa.pft.sandbox;
+
+public class Rectangle {
+  public double a;        // Одна сторона прямоугольника
+  public double b;        // Вторая сторона прямоугольника
+  public Rectangle (double a, double b){
+    this.a = a;
+    this.b = b;
+  }
+}
