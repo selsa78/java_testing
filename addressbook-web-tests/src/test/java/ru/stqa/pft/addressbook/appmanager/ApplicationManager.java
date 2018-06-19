4c8db5fec3af06e328f9c2eea7848a8f3e48af81
Sergey Selivanov <selsa@mail.ru>
2018-06-19 20:48:28 +0300

    Урок2. Общий базовый класс для классов-помощников

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,5 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
@@ -14,15 +12,6 @@ public class ApplicationManager {
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
 
-  public static boolean isAlertPresent(FirefoxDriver wd) {
-      try {
-          wd.switchTo().alert();
-          return true;
-      } catch (NoAlertPresentException e) {
-          return false;
-      }
-  }
-
   public void init() {
     wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true)); //инициализация атрибута объекта
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
@@ -32,8 +21,6 @@ public class ApplicationManager {
     sessionHelper = new SessionHelper(wd);
     sessionHelper.login("admin", "secret"); //сделали метод параметризованным
   }
-
-
 
   public void stop() {
     wd.quit();
